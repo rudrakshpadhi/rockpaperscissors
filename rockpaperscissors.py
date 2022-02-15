c18d9703f2e23ecacc30d4810fc2ec3d4902faec
rudrakshpadhi <99358737+rudrakshpadhi@users.noreply.github.com>
2022-02-15 15:53:54 +0530

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,101 +1,91 @@
 #game of rock paper scissors
 import random
-list=['rock','paper','scissors'] 
+import getpass
+list=['rock','paper','scissors']
+def winner_bot(a,b):
+    if a=='rock'and b=='scissors':
+        return True
+    elif a=='paper'and b=='rock':
+        return True
+    elif a=='scissors'and b=='paper':
+        return True
+    elif b=='rock'and a=='scissors':
+        return False
+    elif b=='paper'and a=='rock':
+        return False
+    elif b=='scissors'and a=='paper':
+        return False
+def winner(a,b):
+    if a=='rock'and b=='scissors':
+        return True
+    elif a=='paper'and b=='rock':
+        return True
+    elif a=='scissors'and b=='paper':
+        return True
+    elif b=='rock'and a=='scissors':
+        return False
+    elif b=='paper'and a=='rock':
+        return False
+    elif b=='scissors'and a=='paper':
+        return False
+players=int(input('do you want to play 1 player or 2 players(1/2)\n'))
 i=0
 j=0
-
-while i<1:
-    players = int(input('how many players do you have(1/2) \n'))
-    if players==1:
+num_wins_user1=0
+num_wins_user2=0
+num_wins_bot=0
+num_wins_user=0
+if players==1:
+    while i<5 and num_wins_user<3 and num_wins_bot<3: 
         bot_option=random.choice(list)
-        user_option=input('enter rock,paper or scissors: \n').lower()
-        print('\n'*50)
-        if user_option!='rock' and user_option!='paper' and user_option!='scissors':
-            print('you can only enter rock,paper or scissors')
+        user_input=input('enter rock,paper or scissors:\n').lower()
+        user_input=user_input.replace(' ','')
+        if user_input in list:
+            if winner_bot(bot_option,user_input)==False:
+                print('you chose '+user_input+' the bot chose '+bot_option)
+                num_wins_user=num_wins_user+1
+                i=i+1
+            elif winner_bot(bot_option,user_input)==True:
+                print('you chose '+user_input+' the bot chose '+bot_option)
+                num_wins_bot=num_wins_bot+1
+                i=i+1
+            elif bot_option==user_input:
+                print('it is a draw')
+            print('the score betweeen you and the bot is',num_wins_user,'-',num_wins_bot)
+        else:
+            print('you did not enter one of the given options')
+    if num_wins_user==3:
+        print('the user has won the best of 5')
+    elif num_wins_bot==3:
+        print('the bot has won the best of 5')
     
-
-        if user_option=='scissors' and bot_option=='paper':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The user has won')
-        elif user_option=='paper' and bot_option=='scissors':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The computer has won')
-        elif user_option=='rock' and bot_option=='scissors':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The user has won')
-        elif user_option=='scissors' and bot_option=='rock':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The computer has won')
-        elif user_option=='paper' and bot_option=='rock':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The user has won')
-        elif user_option=='rock' and bot_option=='paper':
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('The computer has won')
-        elif user_option==bot_option:
-            print('The computer chose: ',bot_option)
-            print('You chose: ',user_option)
-            print('it is a draw')
-        else:
-            print('Something has gone wrong')
-        
-
-
-    elif players==2:
-        user1_option=input('User 1, enter rock,paper or scissors: \n').lower()
+elif players==2:
+    user1=input('User 1, enter your name\n')
+    user2=input('User 2, enter your name\n')
+    while j<5 and num_wins_user1<3 and num_wins_user2<3:
+        user1_input=getpass.getpass(user1+' enter rock,paper or scissors:\n')
+        user1_input=user1_input.replace(' ','')
         print('\n'*50)
-        if user1_option!='rock' and user1_option!='paper' and user1_option!='scissors':
-            print('you can only enter rock,paper or scissors')
+        user2_input=getpass.getpass(user2+' enter rock,paper or scissors:\n')
+        user2_input=user2_input.replace(' ','')
+        if user1_input and user2_input in list:
+            if winner(user1_input,user2_input)==True:
+                print(user1+' chose '+user1_input+' and '+user2+' chose '+user2_input)
+                num_wins_user1=num_wins_user1+1
+                j=j+1
+            elif winner(user1_input,user2_input)==False:
+                print(user1+' chose '+user1_input+' and '+user2+' chose '+user2_input)
+                num_wins_user2=num_wins_user2+1
+                j=j+1
+            elif user1_input==user2_input:
+                print('it is a draw')
+            print('the score betweeen',user1,'and',user2,'is',num_wins_user1,'-',num_wins_user2)
         else:
-            user2_option=input('User 2,enter rock,paper or scissors: \n')
-            print('\n'*50)
-            if user1_option!='rock' and user1_option!='paper' and user1_option!='scissors':
-                print('you can only enter rock,paper or scissors')
-            else:
-                if user1_option=='scissors' and user2_option=='paper':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 1 has won')
-                elif user1_option=='paper' and user2_option=='scissors':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 2 has won')
-                elif user1_option=='rock' and user2_option=='scissors':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 1 has won')
-                elif user1_option=='scissors' and user2_option=='rock':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 2 has won')
-                elif user1_option=='paper' and user2_option=='rock':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 1 has won')
-                elif user1_option=='rock' and user2_option=='paper':
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('User 2 has won')
-                elif user1_option==user2_option:
-                    print('User 1 chose: ',user1_option)
-                    print('User 2 chose: ',user2_option)
-                    print('it is a draw')
-                else:
-                    print('Something has gone wrong')
-    else:
-        print('enter a valid number of players ')
-    play_again=input('do you want to play again? (yes/no)\n').lower()
-    print('\n'*50)
-    if play_again=='no':
-        break
-    elif play_again=='yes':
-        pass
+            print('you did not enter one of the given options')
+    if num_wins_user1==3:
+        print('the user has won the best of 5')
+    elif num_wins_user2==3:
+        print('the bot has won the best of 5')
 
 
 
